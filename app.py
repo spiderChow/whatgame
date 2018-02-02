8320a2324067dfe4bb0577f968928938bc93cf38
chow <511392148@qq.com>
2018-02-02 16:49:20 +0800

    basic login and ranklist

--- app.py
+++ app.py
@@ -1,67 +1,133 @@
 # all the imports
 import os
-import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+     render_template, flash, jsonify
+from flask_wtf import Form
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) # create the application instance :)
 app.config.from_object(__name__) # load config from this file , flaskr.py
+# for DB
+app.config['SECRET_KEY'] = 'i am junying zhou' # protect the FORM from CSRF; TODO: need to be saved into ENV 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:xswqaz@localhost:3306/whatgame'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-# Load default config and override config from an environment variable
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
-))
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-def init_db():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Initializes the database."""
-    init_db()
-    print('Initialized the database.')
+db = SQLAlchemy(app)
 
 
 
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+class Sentence(db.Model):
+    __tablename__ = 'sentences'
+    id = db.Column(db.Integer, nullable=False, primary_key=True, autoincrement=True)
+    # user_id = db.Column(db.Integer, nullable=False) # foreign key???
+    topic = db.Column(db.String(20), nullable=False, server_default='')
+    content = db.Column(db.String(100), nullable=False, server_default='')
+    # relationship:
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
 
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
+    def __repr__(self):
+        return '<SEN %r>' % self.content
+
+class User(db.Model):
+    
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, nullable=False, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(32), nullable=False, unique=True, server_default='', index=True)
+    username = db.Column(db.String(32), nullable=False, server_default='', index=True)
+    score = db.Column(db.Integer, nullable=False, server_default='0')
+    questionamount = db.Column(db.Integer, nullable=False, server_default='0')
+    # relationship
+    sentences = db.relationship('Sentence',backref='sentence')
+
+    def __repr__(self):
+        return '<User %r,Email %r>' %(self.username,self.email)
+
+    def __init__(self,email,username):
+        self.email = email
+        self.username = username
+        self.score = 0
+        self.questionamount = 0
+
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+           'email' : self.email,
+           'username' : self.username,
+           'score' : self.score,
+           'questionamount' : self.questionamount
+        }
+
+# html form
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email
+from flask_bootstrap import Bootstrap
+Bootstrap(app)
+
+# extends FlaskForm
+class LoginForm(FlaskForm):
+    # first para is label
+    email = StringField('Email address')
+    username = StringField('Nickname')
+    submit = SubmitField('Go to play')
+
+
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/howtoplay')
+@app.route('/howtoplay',methods=['GET','POST']) 
 def how_to_play():
-    return render_template('how_to_play.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        username = form.username.data
+        user = User.query.filter_by(email=email).first()
+        if user is None:
+            if username is None or len(username)==0:
+                flash('please name yourself!')
+                return redirect(url_for('how_to_play'))
+            user = User(email=email,username=username)
+            db.session.add(user)
+            db.session.commit()
+            session['known']=False
+        else:
+            session['known']=True
+
+        return redirect(url_for('play'))
+    return render_template('how_to_play.html',form=form)
 
 @app.route('/play')
 def play():
     return render_template('play.html')
 
+@app.route('/_rank_list')
+def rank_list():
+    user_id = request.args.get('user_id', 1, type=int)
+    # get ranking list from DB
+    l = User.query.order_by(User.score)
+    l = [item.serialize for item in l.all()]
+    l.reverse()
+    return jsonify(json_list=l)
+
+@app.route('/dump_sent')
+def dump_sentence():
+    pass
+
+"""
+note for mysql installation:
+  sudo apt-get install libmysqlclient-dev
+  sudo apt-get install python-dev
+  sudo python setup.py install
+
+mysql_config: command not found
+
+"""
 
 if __name__ == '__main__':
     app.run()
